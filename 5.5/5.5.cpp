b8c2d6024d9f648a61fa181df19f98903d7a388e
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-30 19:47:12 +0200

    kih

---
+++ 5.5/5.5.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.05/Lab5.05.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace My55
+{
+	TEST_CLASS(My55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a;
+			int depth = 0;
+			a = Function(1, 6, 1, depth);
+			Assert::AreEqual(a, 2);
+		}
+	};
+}
---
+++ Lab5.05/Lab5.05.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int depth = 0;
+
+double Function(int a, int n, int level, int& depth)
+{
+	if (level > depth)
+		depth = level;
+	cout << "level = " << level << endl;
+	if (n == 1)
+		return 1;
+	else
+	{
+		return  1. / 3 * (2 * Function(a, n - 1, level + 1, depth) +
+			8. / (pow(Function(a, n - 1, level + 1, depth), 2)));
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "n = "; cin >> n;
+	cout << "result = " << Function(1, n, 1, depth) << endl;
+	cout << "depth = " << depth;
+}
